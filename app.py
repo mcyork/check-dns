b2f3e957814458fd6b519cfa1d4f26d91a21f11c
Ian McCutcheon <ian@mcyork.com>
2024-05-28 20:55:47 -0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+import dns.resolver
+
+app = Flask(__name__)
+
+# Route to handle DNS lookup form and results
+@app.route('/dns-lookup', methods=['GET', 'POST'])
+def dns_lookup():
+    if request.method == 'POST':
+        dns_name = request.form['dns_name']
+        dns_type = request.form['dns_type']
+        results = []
+        try:
+            answers = dns.resolver.resolve(dns_name, dns_type)
+            for rdata in answers:
+                results.append(str(rdata))
+        except Exception as e:
+            results.append(str(e))
+        return render_template('dns_results.html', dns_name=dns_name, dns_type=dns_type, results=results)
+    return render_template('dns_form.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
